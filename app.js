be42994275c89a5cb654bd72381485b912455f61
Renan Andrade <eu@renanandrade.com.br>
2017-12-17 12:44:31 -0200

    set player functions

--- app.js
+++ app.js
@@ -2,16 +2,38 @@ new Vue({
 	el: '#app',
 	data: {
 		inGame: false,
-		initialHelth: 100
+		playerHelth: 100,
+		monsterHelth: 100
 	},
-	computed: {
-		playerHelth: function() {
-			total = this.initialHelth;
-			return total + '%'
+	methods: {
+		startGame: function (){
+			this.inGame = true;
 		},
-		monsterHelth: function() {
-			total = this.initialHelth;
-			return total + '%'
+		attack: function (){
+			this.playerHelth -= this.randomAmount();
+			this.monsterHelth -= this.randomAmount();
+		},
+		specialAttack: function (){
+			this.playerHelth -= this.randomAmount();
+			this.monsterHelth -= this.randomAmount(true);
+		},
+		heal: function (){
+			this.playerHelth += this.randomAmount();
+			this.playerHelth -= this.randomAmount();
+			this.monsterHelth -= this.randomAmount();
+		},
+		randomAmount: function (special){
+			min = 1;
+			max = (special?20:6);
+			amout = Math
+				.floor(Math.random() * (max - min)) + min
+
+			return amout;
+		},
+		reset: function (){
+			this.inGame = false,
+			this.playerHelth = 100,
+			this.monsterHelth = 100
 		}
 	}
 });
